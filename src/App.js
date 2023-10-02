fa17e1fc74952930310686e156e3e52cea6bf78e
WIll Stansill <willstansill@gmail.com>
2023-10-02 13:10:48 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,24 +3,18 @@ import { useEffect, useState } from 'react';
 
 import './App.css';
 
-// Refer to the README doc for more information about using API
-// keys in client-side code. You should never do this in production
-// level code.
 const settings = {
   apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
   network: Network.ETH_MAINNET,
 };
 
-
-// In this week's lessons we used ethers.js. Here we are using the
-// Alchemy SDK is an umbrella library with several different packages.
-//
-// You can read more about the packages here:
-//   https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
 const alchemy = new Alchemy(settings);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
+  const [latestBlockTimestamp, setLatestBlockTimestamp] = useState();
+  const [gasPrice, setGasPrice] = useState(); // Add a state variable for gas price
+  const [transactions, setTransactions] = useState([]);
 
   useEffect(() => {
     async function getBlockNumber() {
@@ -28,9 +22,74 @@ function App() {
     }
 
     getBlockNumber();
-  });
+  }, []);
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+  useEffect(() => {
+    async function getLatestBlockTimeStamp() {
+      try {
+        const blockNumber = await alchemy.core.getBlockNumber();
+        const latestBlock = await alchemy.core.getBlock(blockNumber);
+        setLatestBlockTimestamp(new Date(latestBlock.timestamp * 1000).toString());
+      } catch (error) {
+        console.error('Error fetching latest block timestamp:', error);
+      }
+    }
+
+    getLatestBlockTimeStamp();
+  }, []);
+
+  useEffect(() => {
+    async function getGasPrice() {
+      try {
+        const gasPriceWei = await alchemy.core.getGasPrice();
+        // Convert gasPriceWei to Gwei using the Alchemy SDK
+        const gasPriceGwei = gasPriceWei.toString(); // Gas price is already in Gwei
+        setGasPrice(gasPriceGwei + ' Gwei');
+        console.log('Gas Price:', gasPriceGwei + ' Gwei');
+      } catch (error) {
+        console.error('Error fetching gas price:', error);
+      }
+    }
+  
+    getGasPrice();
+  }, []);
+  useEffect(() => {
+    async function fetchTransactions() {
+      try {
+        const blockNumber = await alchemy.core.getBlockNumber();
+        const blockWithTransactions = await alchemy.core.getBlockWithTransactions(blockNumber);
+        setTransactions(blockWithTransactions.transactions);
+      } catch (error) {
+        console.error('Error fetching transactions:', error);
+      }
+    }
+
+    fetchTransactions();
+  }, []); // Empty dependency array to run only once
+
+  return (
+    <div className="App">
+      <h1>Ethereum Block Explorer</h1>
+      <p>Current Block Number: {blockNumber}</p>
+      <p>Latest Block Timestamp: {latestBlockTimestamp}</p>
+      <p>Current Gas Price: {gasPrice}</p>
+
+      {transactions.length > 0 && (
+        <div className="transactions-section">
+          <h2 className="sticky-title">Recent Transactions</h2>
+          <div className="transactions-list-container">
+          <ul className="transactions-list">
+            {transactions.map((tx, index) => (
+              <li key={index} className="transaction-item">
+                Transaction Hash: {tx.hash} - From: {tx.from} - To: {tx.to}
+              </li>
+            ))}
+          </ul>
+        </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
